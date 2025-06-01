4675c1dd7a35dca211a21592053d95be511ebf98
Ashish M Jacob <83330583+ashish-m-jacob@users.noreply.github.com>
2025-06-01 16:54:57 +0530

    update to increase qty

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -24,7 +24,7 @@ const Order = ({ order, setOrder }) => {
   };
 
   const increaseQty = (orderItem, index) => {
-    const items = [JSON.parse(localStorage.getItem("order"))];
+    const items = JSON.parse(localStorage.getItem("order"));
 
     const newQty = orderItem.qty + 1;
 
